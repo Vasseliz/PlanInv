2eeb90e390ac94e9f3e120bea1c4f8fe45314a26
Vasseliz <nicolaszielasko.com>
2026-01-04 00:25:50 -0300

    Criando configurações iniciais

---
+++ PlanInv.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,57 @@
+﻿using PlanInv.Domain.Entities; 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace PlanInv.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    //public DbSet<Usuario> Usuarios { get; set; } = null!;
+    //public DbSet<Ativo> Ativos { get; set; } = null!;
+    //public DbSet<Posicao> Posicoes { get; set; } = null!;
+    //public DbSet<Transacao> Transacoes { get; set; } = null!;
+    //public DbSet<Provento> Proventos { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Aplicar todas as configurações do assembly
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        // Criar o baseENtity
+        
+        var entries = ChangeTracker.Entries()
+            .Where(e => e.Entity is BaseEntity && 
+                       (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+        foreach (var entry in entries)
+        {
+            var entity = (BaseEntity)entry.Entity;
+
+            if (entry.State == EntityState.Added)
+            {
+                entity.CreateAt = DateTime.UtcNow;
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                entity.UpdateAt = DateTime.UtcNow;
+            }
+        }
+        
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+}
