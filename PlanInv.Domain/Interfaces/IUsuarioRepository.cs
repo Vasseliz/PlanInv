db0ad12fd96d1e357b6f34eb52fd17afaa1c2bf7
Vasseliz <nicolaszielasko.com>
2026-01-08 22:05:47 -0300

    implementando repository

---
+++ PlanInv.Domain/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,16 @@
+﻿using PlanInv.Domain.Entities;
+
+namespace PlanInv.Domain.Interfaces;
+
+public interface IUsuarioRepository
+{
+    Task<Usuario> AddAsync(Usuario usuario);
+    Task<Usuario?> GetByIdIncludingInativosAsync(int id);
+    Task<Usuario?> GetByIdAsync(int id);
+    Task<Usuario?> GetByIdWithPosicoesAsync(int id);
+    Task<Usuario?> GetByCpfAsync(string cpf);
+    Task<List<Usuario>> GetAllAsync();
+    Task UpdateAsync(Usuario usuario);
+    Task<bool> ExistsByCpfAsync(string cpf);
+    Task<bool> ExistsAsync(int id);
+}
