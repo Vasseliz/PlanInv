593447f7192bc97984cb9e19409ee9b414dda142
Vasseliz <nicolaszielasko.com>
2026-01-08 22:04:00 -0300

    corrigindo DTOs

--- PlanInv.Application/Dtos/UsuarioResponseDto.cs
+++ PlanInv.Application/Dtos/UsuarioResponseDto.cs
@@ -10,14 +10,15 @@ public class UsuarioResponseDto
 
 {
     public int Id { get; set; }
-    public string Nome { get; set; }
+    public required string Nome { get; set; }
     public int Idade { get; set; }
-    public string Cpf { get; set; }
+    public required string Cpf { get; set; }
     public decimal MetaDeAportesMensal { get; set; }
 
     public DateTime CreatedAt { get; set; }
-
-    public List<PosicaoDto> Posicoes  { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public bool UsuarioAtivo { get; set; }
+    public List<PosicaoDto> Posicoes  { get; set; } = new();
 
     public decimal TotalInvestido => Posicoes?.Sum(p => p.ValorTotal) ?? 0;
     public int QuantidadePosices => Posicoes?.Count ?? 0;
