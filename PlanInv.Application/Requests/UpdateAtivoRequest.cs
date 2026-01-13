4f46937248a1c52943be23f4c412e36f49bce05d
Vasseliz <nicolaszielasko.com>
2026-01-12 21:49:09 -0300

    Requests para a controller de create e update

---
+++ PlanInv.Application/Requests/UpdateAtivoRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlanInv.Application.Requests;
+
+public class UpdateAtivoRequest
+{
+    [Range(0.01, double.MaxValue, ErrorMessage = "Cotação deve ser maior que zero")]
+    public decimal? CotacaoAtual { get; set; }
+
+    public bool TemAlgumCampo() => CotacaoAtual.HasValue;
+}
