0c1fd20b28f9cdb2d558770176f1b2b91abdb562
Vasseliz <nicolaszielasko.com>
2026-01-12 21:46:53 -0300

    Interface de ativos

---
+++ PlanInv.Application/Interfaces/IAtivoService.cs
@@ -0,0 +1,13 @@
+﻿using PlanInv.Application.Dtos;
+using PlanInv.Application.Requests;
+
+namespace PlanInv.Application.Interfaces;
+
+public interface IAtivoService
+{
+    Task<AtivoDto> CreateAtivoAsync(CreateAtivoRequest request);
+    Task<AtivoResponseDto?> GetByIdAsync(int id);
+    Task<AtivoDto?> GetByTickerAsync(string ticker);
+    Task<List<AtivoDto>> GetAllAsync();
+    Task<AtivoDto?> UpdateAtivoAsync(int id, UpdateAtivoRequest request);
+}
